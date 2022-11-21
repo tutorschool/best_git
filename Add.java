1e9aac6b8c4c0e2b98ca71e68ff37135de7e26a1
tutorschool <tutorschool005@gmail.com>
2022-11-21 17:04:48 +0545

    2nd code version

--- Add.java
+++ Add.java
@@ -5,11 +5,17 @@ public class Add
 
        {
 
-          system.out.println(" Hello learning git 1st time ")    
-
+          System.out.println(" Hello learning git 1st time ")    
+          
+          int a = 10;
+          int b = 5;
+          int sum  = a+b;
+          System.out.println(" the 2nd git modification  ")    ;
+           System.out.println(" the sum is " + sum);         
 
        }
    
 
 
 }
+
